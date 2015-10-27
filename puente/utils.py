ef8f6734456206f181d459e35bc4e8f4d9943713
Will Kahn-Greene <willkg@mozilla.com>
2015-10-27 15:13:38 -0400

    Fix generate_keywords to be easier to use
    This makes it easier to add new keywords to the default list.

--- puente/utils.py
+++ puente/utils.py
@@ -1,11 +1,16 @@
 from babel.messages.extract import DEFAULT_KEYWORDS as BABEL_KEYWORDS
 
 
-def generate_keywords():
+def generate_keywords(additional_keywords=None):
     """Generates gettext keywords list"""
     keywords = set(BABEL_KEYWORDS.keys())
     keywords.add('_lazy')
     # FIXME: Add other keywords here
+
+    # Add specified keywords
+    if additional_keywords:
+        for kwd in additional_keywords:
+            keywords.add(kwd)
     return list(keywords)
 
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,19 @@
-from puente.utils import collapse_whitespace
+from puente.utils import collapse_whitespace, generate_keywords
+
+
+def test_generate_keywords():
+    assert (
+        sorted(generate_keywords()) ==
+        ['N_', '_', '_lazy', 'dgettext', 'dngettext', 'gettext', 'ngettext',
+         'pgettext', 'ugettext', 'ungettext']
+    )
+
+    assert (
+        sorted(generate_keywords(['foo', 'foo', 'bar', '_'])) ==
+        ['N_', '_', '_lazy', 'bar', 'dgettext', 'dngettext', 'foo', 'gettext',
+         'ngettext', 'pgettext', 'ugettext', 'ungettext']
+    )
+
 
 
 def test_collapse_whitespace():
